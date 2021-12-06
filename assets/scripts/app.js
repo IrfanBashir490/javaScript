40753c19a3fdbb4e3ee3a721997ea0320dbf3523
IrfanBashir490 <irfanbinbashir490@gmail.com>
2021-12-06 21:28:41 +0530

    changed index .html and added app.js

---
+++ assets/scripts/app.js
@@ -0,0 +1,7 @@
+let currentResult = 0;
+
+currentResult = (currentResult + 10) * 3 / 2 - 1;
+
+let calculationDescriptio = '(' + currentResult +' + 10) * 3 / 2 - 1';
+
+outputResult(currentResult, calculationDescriptio);
